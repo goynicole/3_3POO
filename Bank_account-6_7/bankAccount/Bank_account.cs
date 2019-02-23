d4272a70e0c030b492b16b4f4a6a4876a3a1e00e
goy <goy.nicole018@gmail.com>
2019-02-23 15:25:27 +0100

    POO

---
+++ Bank_account-6_7/bankAccount/Bank_account.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankAccount
+{
+    public class Bank_account
+    {
+        public String holder;
+        private String currency = "euros";
+        public Double balance;
+
+        public string Currency { get => currency; set {
+                if (value == "dollars")
+                { currency = value; }
+            }
+        }
+
+        /// <summary>
+        /// Le constructeur nous permet d'hydrater l'objet lors de sa création.
+        /// </summary>
+        /// <param name="_holder"> Nom du propriétaire du compte.</param>
+        /// <param name="_currency">Devise du compte</param>
+        /// <param name="_balance">Solde actuel du compte</param>
+        public Bank_account(String _holder, String _currency, Double _balance)
+        {
+            holder = _holder;
+            Currency = _currency;
+            balance = _balance;
+        }
+        /// <summary>
+        /// Cette méthode permet de créditer le compte bancaire
+        /// </summary>
+        /// <param name="amount">C'est le montant en euros à créditer</param>
+        public void credit(double amount)
+        {
+            // "this" evite les confusions lors du choix des var
+            this.balance = this.balance + amount;            
+        }
+
+        public void debit(double amount)
+        {
+            this.balance = this.balance - amount;
+        }
+    }
+}
---
+++ Bank_account-6_7/bankAccount/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Création de l'objet avec le constructeur new + On va hydrater l'objet
+            Bank_account bank = new Bank_account("Gaetan", "euros", 20000);
+            bank.Currency = "yen";
+            Console.WriteLine("Bonjour monsieur, " + bank.holder + " le solde de votre compte est de " + bank.balance + bank.Currency);
+            bank.credit(1000);
+            Console.WriteLine("Le nouveau solde de votre compte est de : " + bank.balance);
+            Console.ReadKey();
+
+        }
+    }
+}
